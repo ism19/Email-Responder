c6dab936e26a3174770f4432779f65a61e019cfd
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-07-11 17:17:44 -0500

    added polling task to FastAPI lifespan on startup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from app.routes import email_routes, syllabus
+from app.services.polling import poll_gmail
+import asyncio
+import os
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    asyncio.create_task(poll_gmail (
+        os.environ.get("PROFESSOR_EMAIL"),
+        os.environ.get("USER_ID")
+    ))
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(email_routes.router)
 app.include_router(syllabus.router)
